c336bf9c40f212bb58e90c5c3b5bca08bbb639a0
0SiD <damianesmoris@gmail.com>
2021-10-01 14:46:53 -0300

    Se agrega el archivo Funciones.java

---
+++ Obligatorio/src/program/Funciones.java
@@ -0,0 +1,40 @@
+package program;
+
+public class Funciones {
+	public static int inicar_sesion(){
+		
+		// Se inicializan las variables para las entradas del usuario.
+		
+		String usuario_ingresado;
+		int pin_ingresado;
+		
+		while (true){
+			
+		// El usuario ingresa los datos.
+			
+		 System.out.print("Ingrese el nombre de usuario: ");
+		 usuario_ingresado = Main.sc.next().toLowerCase();
+		 
+		 System.out.print("Ingrese el pin: ");
+		 pin_ingresado = Main.sc.nextInt();
+		 
+		// Se comprueba si los datos ingresados están en la lista.
+		 
+		 for (String x: Main.usuarios) {
+			 if (usuario_ingresado.equals(x) && pin_ingresado == Main.pines.get(Main.usuarios.indexOf(usuario_ingresado))) {
+				 
+				 System.out.println("\nHaz iniciado sesión como %s.\n".formatted(usuario_ingresado));
+				 return Main.usuarios.indexOf(usuario_ingresado);	
+				 
+			}else{
+				
+				// Si no es así se da un mensaje de error.
+				System.out.print("\nEl usuario y/o el pin ingresado son incorrectos");
+				Utilidades.TresPuntos();
+			} // else.
+		 } // for.
+		
+		} // while.
+	
+	} // función.
+}
--- Obligatorio/src/program/Main.java
+++ Obligatorio/src/program/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
 	
+	// Variables.
+	
 	static ArrayList<String> usuarios = new ArrayList<>();
 	static ArrayList<Integer> pines = new ArrayList<>();
 	static Scanner sc = new Scanner(System.in);
@@ -13,10 +15,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		// Se crea el usuario administrador.
 		
-		// Variables.
 		usuarios.add("administrador");
-		pines.add(1234);
+		pines.add(pin_defecto);
 		
 		Funciones.inicar_sesion();
 		
