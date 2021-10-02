4ea7c29b3fe0acae92db9206e223074202943271
0SiD <damianesmoris@gmail.com>
2021-10-02 00:50:10 -0300

    Se agregaron los 5 usuarios por defecto de manera aleatoria.

--- Obligatorio/src/program/Main.java
+++ Obligatorio/src/program/Main.java
@@ -1,6 +1,9 @@
 package program;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -15,10 +18,34 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		
 		// Se crea el usuario administrador.
 		
 		usuarios.add("administrador");
 		pines.add(pin_defecto);
+		
+		// Se crea una lista y se le añaden los posibles nombres de los usuarios.
+		
+		List<String> nombres = new ArrayList<String>();
+		nombres.addAll(Arrays.asList("juan", "pablo", "ana", "luis", "sofia", "antonella", "rosario", "josé"));
+		
+		// Se inicializan las variables.
+		
+		Random aleatorio = new Random();
+		int valor_maximo = nombres.size();
+		int numero_aleatorio;
+		
+		// Se itera este pedazo de código por la cantidad de usuarios deseados.
+		
+		for (int i = 0; i < 5; i++) {
+			numero_aleatorio = aleatorio.nextInt(valor_maximo - 0) + 0;
+			
+			usuarios.add(nombres.get(numero_aleatorio));				// Se elige un nombre de manera aleatoria.
+			pines.add(aleatorio.nextInt(9999 - 1111) + 0001);			// Se crea un pin de 4 dígitos aleatorio.
+			
+			valor_maximo--;
+			nombres.remove(numero_aleatorio);
+		}
 		
 		Funciones.inicar_sesion();
 		
